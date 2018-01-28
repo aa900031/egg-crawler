d7fa168e97f943da69277dd2358b3c27f9aa865a
zhong666 <aa900031@gmail.com>
2018-01-28 13:10:47 +0800

    改變 Crawler class 寫法&TS定義檔

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,7 @@
 import { BaseContextClass } from 'egg';
+import scrapeIt from 'scrape-it'
 
 export class Crawler extends BaseContextClass {
+  scraper: typeof scrapeIt;
   exec<T>(): Promise<T>;
 }
